cf87a337632a8ce250a24449988dc749427a5704
Phillip Samuel <phillipsamuelk3@gmail.com>
2018-04-12 15:31:15 +0300

    Hey, my test worksgit help push ^^)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # hello-world
 First try at this!
 Superstars only
+I've added this to be sure
---
+++ test_code.py
@@ -0,0 +1,13 @@
+#This is test code to see how to use GitHub and git ofcourse.
+
+print("hello world")
+
+for i in xrange(15):
+    if (i%3 == 0):
+        print "I love you in an odd way"
+    else if (i%4 == 0):
+        print "I love you twice as good!"
+    else if (i%10 == 0):
+        print "I love you ten-fold!"
+    else:
+        print "I love you"
